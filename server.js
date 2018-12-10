e7149c1ff719fe08fd0517136260e8ecab3d3474
nathphoenix <nathphoenix75@gmail.com>
2018-12-10 21:30:15 +0100

    modified

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items')
+
 const app = express();
 
 //bodyParser middleware
@@ -18,6 +20,8 @@ mongoose
      .then(() =>  console.log('mongodb connected successfully...'))
      .catch(err => console.log(err));
 
+    //use routes
+    app.use('/api/items', items);
 
      const port = process.env.PORT || 5000;
     //  app.listen(port, () => console.log('server started on port ${port}'));

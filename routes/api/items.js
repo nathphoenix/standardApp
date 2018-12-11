91510ca56f83f301d8158a05baffc4b20449a7c6
nathphoenix <nathphoenix75@gmail.com>
2018-12-11 07:49:56 +0100

    api function upadted

--- routes/api/items.js
+++ routes/api/items.js
@@ -15,4 +15,30 @@ router.get('/', (req, res ) =>{
     .then(items => res.json(items));
 })
 
+//route POST api/items
+//desc CREATE an item
+//@access public
+
+router.post('/', (req, res ) =>{
+    const newitem = new Item({                    //this is in connection with the item created in model direcrory
+       name: req.body.name                     //this is one of the object created in model, date is not included because it was set to default 
+    });
+
+    newitem.save().then(item => res.json(item));             //then give us back the item that is saving
+})
+
+//route DELETE api/items/:id       the delete will have an id that is the route has changed compare to post and get requset
+//desc DELETE an item
+//@access public
+
+router.delete('/:id', (req, res ) =>{    //like a placeholder wi9th an id
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success:true})))
+    .catch(err => res.status(404).json({success:false}));
+    
+   
+});
+
+    
+
 module.exports = router;
